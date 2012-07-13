60f1cd65f6accbe3b60e5701234ff6c35cc0bd9c
unome <agadde01@apuroopgadde>
2012-07-13 19:42:53 -0400

    added new details.xml

---
+++ src/com/apuroopgadde/diagnosticquestions/DetailsActivity.java
@@ -0,0 +1,17 @@
+package com.apuroopgadde.diagnosticquestions;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailsActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+        
+	}
+	
+
+}
--- src/com/apuroopgadde/diagnosticquestions/MainActivity.java
+++ src/com/apuroopgadde/diagnosticquestions/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
@@ -14,10 +16,24 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+        
+    }*/
+    
+    public void onClickHandler(View v)
+    {
+    	if(v.getId()==R.id.button_prevQuestion)
+    	{
+    		//Navigate to previous question
+    	}
+    	if(v.getId()==R.id.button_skipQuestion)
+    	{
+    		//skip to next question 
+    		
+    	}
     }
 
     
